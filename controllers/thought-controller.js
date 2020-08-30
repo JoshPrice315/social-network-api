9594f4e042d778311b141d164004325799e0e0da
JoshPrice315 <61852564+JoshPrice315@users.noreply.github.com>
2020-08-30 15:33:19 -0700

    final

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -34,7 +34,7 @@ const thoughtController = {
       .then(({ _id }) => {
         return User.findOneAndUpdate(
           { _id: params.userId },
-          { $push: { thoughts: _id }},
+          { $push: { thoughts: _id } },
           { new: true, runValidators: true }
         );
       })
@@ -51,7 +51,7 @@ const thoughtController = {
       });
   },
   updateThought({ params, body }, res) {
-    Thought.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true })
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .populate({
         path: 'reactions',
         select: '-__v'
@@ -66,28 +66,27 @@ const thoughtController = {
       })
       .catch(err => {
         console.log(err);
-        // was curious about err codes, my general understanding for RESTful API is 400 is client side error, 500 is server side
         res.status(500).json(err);
       });
   },
   deleteThought({ params }, res) {
     Thought.findOneAndDelete({ _id: params.id })
-    .then(dbThoughtData => {
-      if (!dbThoughtData) {
-        res.status(404).json({ message: `No thought found with ID: ${params.id}` });
-        return;
-      }
-      res.json(dbThoughtData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: `No thought found with ID: ${params.id}` });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
   addReaction({ params, body }, res) {
     Thought.findOneAndUpdate(
       { _id: params.thoughtId },
-      { $push: { reactions: body }},
+      { $push: { reactions: body } },
       { new: true, runValidators: true }
     )
       .populate({
@@ -110,20 +109,20 @@ const thoughtController = {
   deleteReaction({ params }, res) {
     Thought.findOneAndUpdate(
       { _id: params.thoughtId },
-      { $pull: { reactions: { reactionId: params.reactionId } }},
+      { $pull: { reactions: { reactionId: params.reactionId } } },
       { new: true }
     )
-    .then(dbReactionData => {
-      if (!dbReactionData) {
-        res.status(404).json({ message: `No reaction found with ID: ${params.reactionID}` });
-        return;
-      }
-      res.json(dbReactionData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then(dbReactionData => {
+        if (!dbReactionData) {
+          res.status(404).json({ message: `No reaction found with ID: ${params.reactionID}` });
+          return;
+        }
+        res.json(dbReactionData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   }
 }
 
