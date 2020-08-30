815261273f18417e3e8d1765f3a863cb29a326b5
JoshPrice315 <61852564+JoshPrice315@users.noreply.github.com>
2020-08-30 15:09:54 -0700

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./routes'));
+
+// 18.1.5
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
